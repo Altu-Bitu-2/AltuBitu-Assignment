24f4f01769fe01e01e27b227a9f0c14cc5d88796
hyosin Jang <cdnnnl@gmail.com>
2022-03-07 23:47:10 +0900

    [정렬] 3월 6일 업데이트

--- 03월 04일 - 정렬/11651.cpp
+++ 03월 04일 - 정렬/11651.cpp
@@ -6,8 +6,7 @@ using namespace std;
 bool cmp(pair<int, int> p1, pair<int, int> p2) {
 	if (p1.second != p2.second)
 		return p1.second < p2.second;
-	else
-		return p1.first < p2.first;
+	return p1.first < p2.first;
 }
 int main() {
 	ios::sync_with_stdio(false);
@@ -25,6 +24,6 @@ int main() {
 
 	for (int i = 0; i < n; i++) 
 		cout << v[i].first << " " << v[i].second << "\n";
-        
+	
 	return 0;
 }
