e7dc0c151190e001241fabcb7f23d538c322d211
hyosin Jang <cdnnnl@gmail.com>
2022-03-26 19:27:26 +0900

    [브루트포스] 3월 26일 업데이트

--- 03월 18일 - 브루트포스/1018.cpp
+++ 03월 18일 - 브루트포스/1018.cpp
@@ -4,25 +4,17 @@ using namespace std;
 
 char arr[51][51];
 
-// i, j에서 시작하는 8x8 체스판에서 왼쪽 위가 W로 시작할 때 기준으로 계산 
-// 다시 칠해야 하는 최소 타일 수 리턴하는 함수 (num)
+// i, j에서 부터 8x8까지 왼쪽 위가 W로 시작할 때 기준으로 계산 
+// num: 다시 칠해야 하는 타일 수
 // num과 반대 경우의 수인 64-num 중 작은 수 리턴
 int checkWB(int x, int y) {
 	int num = 0;
 	for (int i = x; i <= x + 7; i++) 
 		for (int j = y; j <= y + 7; j++) {
-			if (i % 2 == 0) {
-				if (j % 2 == 0)
-					if (arr[i][j] == 'B') num++; // W여야 함, 근데 B라면 num++
-				else 
-					if (arr[i][j] == 'W') num++; // B여야 함
-			}
-			else {
-				if (j % 2 == 0) 
-					if (arr[i][j] == 'W') num++; // B여야 함
-				else
-					if (arr[i][j] == 'B') num++; // W여야 함
-			}	
+			if (i % 2 == 0 && j % 2 == 0 && arr[i][j] == 'B') num++;	
+			else if (i % 2 == 0 && j % 2 != 0 && arr[i][j] == 'W') num++;
+			else if (i % 2 != 0 && j % 2 == 0 && arr[i][j] == 'W') num++;
+			else if (i % 2 != 0 && j % 2 != 0 && arr[i][j] == 'B') num++;
 		}
 	num = min(num, 64 - num);
 	return num;
