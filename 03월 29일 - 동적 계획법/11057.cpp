4a5b4edb32766b888d86ce1ecf6d5e7eecdaacf9
hyosin Jang <cdnnnl@gmail.com>
2022-04-04 17:12:27 +0900

    [동적 계획법] 4월 4일 업데이트

--- 03월 29일 - 동적 계획법/11057.cpp
+++ 03월 29일 - 동적 계획법/11057.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 const int MOD = 10007;
-int dp[1001][10];
+vector<vector<int>> dp(1001, vector<int>(10, 1));
 int main() {
 
 	int n, answer=0;
 	cin >> n;
 
-	for (int i = 1; i <= n; i++) 
-		for (int j = 0; j <= 9; j++) {
-			if (i == 1) dp[i][j] = 1;
-
-			for (int k = 0; k <= j; k++) {
-				dp[i][j] += dp[i - 1][k];
-				dp[i][j] %= MOD;
-
-			}
+	// dp[i][j]: 길이가 i고 j로 끝나는 오르막 수의 개수
+	for (int i = 2; i <= n; i++) 
+		for (int j = 1; j <= 9; j++) {
+			// dp[i][j] = 길이가 i고 j-1로 끝나는 오르막 수 개수 + 길이가 i-1이고 j로 끝나는 오르막 수 개수
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+			dp[i][j] %= MOD;
 		}
-	
-
-	for (int i = 0; i <= 9; i++)
+	for (int i = 0; i < 10; i++)
 		answer += dp[n][i];
-
 	cout << answer % MOD << "\n";
 
 	return 0;
